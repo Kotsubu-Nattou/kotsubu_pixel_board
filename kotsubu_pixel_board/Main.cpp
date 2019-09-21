1356bd4001781f3fd40a6a260023690e45762eea
kotsubu <@desktop>
2019-09-21 16:47:38 +0900

    スタート

---
+++ kotsubu_pixel_board/Main.cpp
@@ -0,0 +1,59 @@
+﻿#include <Siv3D.hpp>
+#include "kotsubu_pixel_board.h"
+
+
+
+void Main()
+{
+    Font font = Font(24);
+    double resoScale = 30.0, drawScale = 1.0;
+    KotsubuPixelBoard board(resoScale);
+   
+
+
+	while (System::Update())
+	{
+        // ピクセルボードをクリア
+        if (MouseR.down()) {
+            board.clear();
+        }
+        
+
+        // ピクセルボードをレンダリング
+        if (MouseL.pressed()) {
+            // カーソル位置 ⇒ イメージの添え字
+            Vec2 pos = Cursor::Pos() - board.mPos;  // カーソル座標にピクセルボード位置を適応
+            pos /= resoScale;                       // 解像度スケールを適応
+            pos /= drawScale;                       // 描画スケールを適応
+            s3d::Point point(pos.asPoint());        // イメージの添え字
+
+            // 有効な添え字なら、イメージに点を書き込む
+            if (point.x >= 0 && point.x < board.mImg.width() &&
+                point.y >= 0 && point.y < board.mImg.height())
+                board.mImg[point].set(Palette::Cyan);
+        }
+        
+
+        // ピクセルボードをドロー
+        s3d::RenderStateBlock2D renderState(s3d::BlendState::Additive, s3d::SamplerState::ClampNearest);
+        board.draw();
+
+
+        // GUI
+        renderState = { s3d::BlendState::Default, s3d::SamplerState::Default2D };
+        font(U"mPos.x:", board.mPos.x).draw(Vec2(Window::Width() - 200, 0));
+        s3d::SimpleGUI::Slider(board.mPos.x, -200.0, 200.0, Vec2(Window::Width() - 200, 30), 200);
+        font(U"mPos.y:", board.mPos.y).draw(Vec2(Window::Width() - 200, 60));
+        s3d::SimpleGUI::Slider(board.mPos.y, -200.0, 200.0, Vec2(Window::Width() - 200, 90), 200);
+        font(U"drawScale:", drawScale).draw(Vec2(Window::Width() - 200, 120));
+        if (s3d::SimpleGUI::Slider(drawScale, 0.0, 10.0, Vec2(Window::Width() - 200, 150), 200))
+            board.setDrawScale(drawScale);
+        if (s3d::SimpleGUI::Button(U"Reset", Vec2(Window::Width() -150, 185))) {
+            drawScale = 1.0;
+            board.setDrawScale(drawScale);
+        }
+        font(U"resoScale:", resoScale).draw(Vec2(Window::Width() - 200, 220));
+        if (s3d::SimpleGUI::Slider(resoScale, 1.0, 100.0, Vec2(Window::Width() - 200, 250), 200))
+            board.setResoScale(resoScale);  // 連続的にコールされたときのエラーはsiv3dの仕様
+    }
+}
---
+++ kotsubu_pixel_board/kotsubu_pixel_board.h
@@ -0,0 +1,112 @@
+/**************************************************************************************************
+【ヘッダオンリークラス】kotsubu_pixel_board
+
+・概要
+ドットのお絵かきフィールドを提供するクラス（OpenSiv3D専用）
+解放は不要（アプリケーション終了時に自動）
+
+・使い方
+#include <Siv3D.hpp>
+#include "kotsubu_pixel_board.h"
+KotsubuPixelBoard board(8.0);  // 解像度スケールが8.0（ドットの大きさ）のお絵かきフィールドを生成
+メインループ
+    if (flag) setResoScale(20.0);               // 解像度スケールを変更（フィールドは白紙に戻る）
+    board.clear();                              // フィールドを白紙に戻す
+    int w = board.mImg.width();                 // メンバmImgはフィールドの内容。s3d::Image型
+    s3d::Point point = カーソル等の座標をフィールド座標にしたもの;
+    board.mImg[point].set(s3d::Palette::Cyan);  // 点をレンダリング（添え字範囲に注意！）
+    board.mPos = { 0.0, 5.0 };                  // フィールドをずらす。s3d::Vec2型
+    board.setDrawScale(2.0);                    // ドロー時のズーム（縮小時はフィールドが見切れる）
+    board.draw();                               // ドロー
+**************************************************************************************************/
+
+#pragma once
+#include <Siv3D.hpp>
+
+
+
+class KotsubuPixelBoard
+{
+    // 【内部フィールド】
+    double              mDrawScale;
+    double              mResoScale;
+    s3d::Image          mBlankImg;
+    s3d::DynamicTexture mTex;
+
+
+
+public:
+    // 【公開フィールド】
+    s3d::Vec2  mPos;  // ピクセルボードの左上位置
+    s3d::Image mImg;  // 描画イメージ。これに直接.set()などで書き込む
+
+
+
+    // 【コンストラクタ】
+    KotsubuPixelBoard(double resoScale)
+    {
+        setDrawScale(1.0);
+        setResoScale(resoScale);
+    }
+
+
+
+    // 【メソッド】描画イメージを白紙に戻す
+    // 現在の解像度スケールに応じた高速なクリア。また、似たような用途として
+    // s3d::Imageのclear()があるが内容が破棄されてしまう。fill()は負荷が高い
+    void clear()
+    {
+        // 描画イメージをブランクイメージで置き換える（この方法が高速）
+        mImg = mBlankImg;
+    }
+
+
+
+    // 【セッタ】解像度スケール
+    void setResoScale(double scale)
+    {
+        static double oldScale = -1;
+        if (scale < 1.0) scale = 1.0;
+        if (scale == oldScale) return;
+        mResoScale = scale;
+
+        // 新しいサイズのブランクイメージを作る
+        double rate = 1.0 / mResoScale;
+        mBlankImg = s3d::Image(static_cast<size_t>(s3d::Window::Width()  * rate),
+                               static_cast<size_t>(s3d::Window::Height() * rate));
+
+        // 動的テクスチャは「同じサイズ」のイメージを供給しないと更新されないため一旦解放。
+        // ＜補足＞ テクスチャやイメージのrelease()やclear()と、draw()が別所の場合、
+        // 「無い物」のアクセス発生に注意する。また、テクスチャ登録などの重い処理を
+        // 連続で行った場合にエラーすることがあるので注意する。
+        mTex.release();
+
+        // 描画イメージをクリア。
+        // 描画スケールの変更では内容は変えない方が自然。解像度スケールの
+        // 変更ではリセットした方がロジックが簡単になってよい
+        mImg = mBlankImg;
+
+        oldScale = mResoScale;
+    }
+
+
+
+    // 【セッタ】描画スケール
+    void setDrawScale(double scale)
+    {
+        if (scale < 0.0) scale = 0.0;
+        mDrawScale = scale;
+    }
+
+
+
+    // 【メソッド】ドロー
+    void draw()
+    {
+        // 動的テクスチャを更新（同じ大きさでないと更新されない）
+        mTex.fill(mImg);
+
+        // 動的テクスチャをスケーリングしてドロー
+        mTex.scaled(mResoScale * mDrawScale).draw(mPos);
+    }
+};
