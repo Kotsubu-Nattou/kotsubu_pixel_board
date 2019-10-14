0265592b4d3c5f465f7b718b39d537b65b257241
kotsubu <@desktop>
2019-10-15 03:32:11 +0900

    追加: render系メソッド, ブレンドモード機能 v1.3

--- kotsubu_pixel_board/Main.cpp
+++ kotsubu_pixel_board/Main.cpp
@@ -9,6 +9,93 @@
 
 
 
+//void Main()
+//{
+//    size_t width = 200, height = 150;
+//    double scale = 15.0;
+//    KotsubuPixelBoard board(width, height, scale);
+//    bool isScroll = false;
+//    Vec2 scrollStartPos;
+//    bool isDrawing = false;
+//    Point drawingStartPos;
+//    Font font = Font(24);
+//    int  guiAreaLeft = Window::Width() - 220;
+//    size_t blendMode = (size_t)KotsubuPixelBoard::EnumBlendMode::Default;
+//
+//
+//
+//    while (System::Update())
+//    {
+//        // 右ドラッグでスクロール
+//        if (MouseR.down() && (Cursor::Pos().x < guiAreaLeft)) {
+//            isScroll = true;
+//            scrollStartPos = board.mBoardPos - Cursor::Pos();
+//        }
+//
+//        if (MouseR.up())
+//            isScroll = false;
+//        
+//        if (isScroll)
+//            board.mBoardPos = scrollStartPos + Cursor::Pos();
+//
+//
+//
+//        // 左ドラッグで線分をレンダリング
+//        if (MouseL.down() && (Cursor::Pos().x < guiAreaLeft)) {
+//            isDrawing = true;
+//            // カーソル座標をイメージ座標に変換
+//            drawingStartPos = board.toImagePos(Cursor::Pos());
+//        }
+//
+//        if (MouseL.up() && isDrawing) {
+//            Point pos = board.toImagePos(Cursor::Pos());
+//            // 有効範囲ならレンダリング
+//            if (board.checkRange(pos))
+//                board.renderLineAA(drawingStartPos, pos, ColorF(0.3, 1.0, 0.7, 0.7));
+//            isDrawing = false;
+//        }
+//        
+//        
+//        
+//        // ピクセルボードをドロー
+//        s3d::RenderStateBlock2D renderState(s3d::BlendState::Default, s3d::SamplerState::ClampNearest);
+//        board.draw();
+//
+//
+//
+//        // GUI関係
+//        renderState = { s3d::BlendState::Default, s3d::SamplerState::Default2D };
+//
+//        Rect(guiAreaLeft, 0, Window::Width() - guiAreaLeft, Window::Height()).draw(Palette::Dimgray);
+//
+//        if (SimpleGUI::Button(U"Clear", Vec2(Window::Width() - 160, 10)))
+//            board.clear();
+//
+//        font(U"Pos: ", (int)board.mBoardPos.x, U", ", (int)board.mBoardPos.y).draw(Vec2(Window::Width() - 210, 50));
+//
+//        double w = width, h = height;  // スライダー用に型を変換
+//        font(U"Width: ", width).draw(Vec2(Window::Width() - 210, 80));
+//        SimpleGUI::Slider(w, 0.0, 400.0, Vec2(Window::Width() - 210, 110), 200);
+//
+//        font(U"Height: ", height).draw(Vec2(Window::Width() - 210, 150));
+//        SimpleGUI::Slider(h, 0.0, 300.0, Vec2(Window::Width() - 210, 180), 200);
+//
+//        width = w; height = h;
+//        board.changeSize(width, height);
+//
+//        font(U"Scale: ", scale).draw(Vec2(Window::Width() - 210, 220));
+//        if (SimpleGUI::Slider(scale, 0.0, 100.0, Vec2(Window::Width() - 210, 250), 200))
+//            board.setScale(scale);
+//
+//        if (SimpleGUI::RadioButtons(blendMode, { U"Default", U"Alpha", U"Add", U"AddSoft", U"Mul" }, Vec2(Window::Width() - 210, 300)))
+//            board.blendMode((KotsubuPixelBoard::EnumBlendMode)blendMode);
+//    }
+//}
+
+
+
+
+
 void Main()
 {
     size_t width = 800, height = 600;
@@ -17,6 +104,7 @@ void Main()
     Font font = Font(64, s3d::Typeface::Bold);
     Stopwatch time;
     int max = 100000000;
+    board.blendMode(KotsubuPixelBoard::EnumBlendMode::Default);
 
 
     while (System::Update())
@@ -28,10 +116,13 @@ void Main()
             board.blendMode(KotsubuPixelBoard::EnumBlendMode::Alpha);
 
         if (s3d::Key3.pressed())
-            board.blendMode(KotsubuPixelBoard::EnumBlendMode::Add);
+            board.blendMode(KotsubuPixelBoard::EnumBlendMode::Additive);
 
         if (s3d::Key4.pressed())
-            board.blendMode(KotsubuPixelBoard::EnumBlendMode::Add2);
+            board.blendMode(KotsubuPixelBoard::EnumBlendMode::AdditiveSoft);
+
+        if (s3d::Key5.pressed())
+            board.blendMode(KotsubuPixelBoard::EnumBlendMode::Multiple);
 
         board.clear();
         ColorF col = { RandomColorF(), Random(1.0) };
@@ -73,89 +164,11 @@ void Main()
             board.renderDot(pos, col);
         }
         time.pause();
-        
+
 
         // ピクセルボードをドロー
         RenderStateBlock2D renderState(BlendState::Default, SamplerState::ClampNearest);
         board.draw();
         font(time.ms(), U" ms").draw(Point(10, 0));
-        font(U"BlendMode: ", (int)board.mBlendMode).draw(Point(10, 70));
     }
 }
-
-
-
-//void Main()
-//{
-//    size_t width = 40, height = 30;
-//    double scale = 20.0;
-//    KotsubuPixelBoard board(width, height, scale);
-//    bool isScroll = false;
-//    Vec2 scrollStartPos;
-//    Font font = Font(24);
-//    int  guiAreaLeft = Window::Width() - 220;
-//
-//
-//
-//    while (System::Update())
-//    {
-//        // ピクセルボードをドラッグしてスクロール
-//        if (MouseR.down() &&
-//            Cursor::Pos().x < guiAreaLeft) {
-//            isScroll = true;
-//            scrollStartPos = board.mBoardPos - Cursor::Pos();
-//        }
-//
-//        if (MouseR.up())
-//            isScroll = false;
-//        
-//        if (isScroll)
-//            board.mBoardPos = scrollStartPos + Cursor::Pos();
-//
-//
-//
-//        // ピクセルボードをレンダリング
-//        if (MouseL.pressed() &&
-//            Cursor::Pos().x < guiAreaLeft) {
-//            static Point oldPos;
-//            // カーソル座標をイメージ座標に変換
-//            Point pos = board.toImagePos(Cursor::Pos());
-//            // 座標更新、かつ有効範囲なら
-//            if ((pos != oldPos) && board.checkRange(pos))
-//                board.renderDot_add(pos, ColorF(0.2, 1.0, 0.7, 0.7));
-//            oldPos = pos;
-//        }
-//        
-//
-//
-//        // ピクセルボードをドロー
-//        s3d::RenderStateBlock2D renderState(s3d::BlendState::Default, s3d::SamplerState::ClampNearest);
-//        board.draw();
-//
-//
-//
-//        // GUI関係
-//        renderState = { s3d::BlendState::Default, s3d::SamplerState::Default2D };
-//
-//        Rect(guiAreaLeft, 0, Window::Width() - guiAreaLeft, Window::Height()).draw(Palette::Dimgray);
-//
-//        if (s3d::SimpleGUI::Button(U"Clear", Vec2(Window::Width() - 160, 20)))
-//            board.clear();
-//
-//        font(U"Pos: ", (int)board.mBoardPos.x, U", ", (int)board.mBoardPos.y).draw(Vec2(Window::Width() - 210, 70));
-//
-//        double w = width, h = height;  // スライダー用に型を変換
-//        font(U"Width: ", width).draw(Vec2(Window::Width() - 210, 110));
-//        s3d::SimpleGUI::Slider(w, 0.0, 100.0, Vec2(Window::Width() - 210, 140), 200);
-//
-//        font(U"Height: ", height).draw(Vec2(Window::Width() - 210, 180));
-//        s3d::SimpleGUI::Slider(h, 0.0, 100.0, Vec2(Window::Width() - 210, 210), 200);
-//
-//        width = w; height = h;
-//        board.setSize(width, height);  // 連続的に異なるサイズを適用すると負荷が高く、エラーするので注意
-//
-//        font(U"Scale: ", scale).draw(Vec2(Window::Width() - 210, 250));
-//        if (s3d::SimpleGUI::Slider(scale, 0.0, 100.0, Vec2(Window::Width() - 210, 280), 200))
-//            board.setScale(scale);
-//    }
-//}
--- kotsubu_pixel_board/kotsubu_pixel_board.h
+++ kotsubu_pixel_board/kotsubu_pixel_board.h
@@ -1,30 +1,43 @@
 /**************************************************************************************************
-【ヘッダオンリークラス】kotsubu_pixel_board v1.2
+【ヘッダオンリークラス】kotsubu_pixel_board v1.3
 
-・概要
-ドットのお絵かきボードを提供するクラス（OpenSiv3D専用）
-1つのインスタンスにつき、1つの描画用イメージを内包する構造。
-座標系       --- クライアント左上を原点とするボードの位置, ボード左上を原点とする描画イメージの座標
-レンダリング --- クラスの公開フィールド s3d::Image mImg に対して直接書き込む（高速化というか手抜き）
+◎概要
+2Dドットのお絵かきボードを提供するクラス（OpenSiv3D専用）
+1つのインスタンスにつき、1つのイメージを内包する構造。
 明示的な解放は不要。
 
-・使い方
+・座標系
+  1. クライアントの左上を原点とした「ボードの位置」 --- s3d::Vec2型
+  2. ボードの左上を原点とした「イメージの座標」     --- s3d::Point型
+  また、カーソル座標をイメージ座標に変換するメソッド等も用意。
+
+・レンダリング
+  図形の種類別にrender〇〇メソッドを用意。事前にブレンドモードを指定してレンダリングを行う。
+  また、イメージ（s3d::Image mImg）は「公開」しており、通常のイメージに対する操作が可能。
+
+
+◎使い方
 #include <Siv3D.hpp>
 #include "kotsubu_pixel_board.h"
-KotsubuPixelBoard board(32, 24, 10.0);         // 32x24ドット、ズーム率10のお絵かきボードを生成
+KotsubuPixelBoard board(32, 24, 10.0);      // 32x24ドット、ズーム率10のお絵かきボードを生成
+
 メインループ
-    board.clear();                             // ボードを白紙にする
-    int w = board.mImg.width();                // 公開メンバmImgはボードの描画内容（s3d::Image型）
+    board.clear();                          // ボードを白紙にする
+    int w = board.mImg.width();             // イメージの幅を取得（通常のs3d::Imageと同様の操作が可能）
+    board.blendMode(KotsubuPixelBoard::EnumBlendMode::Alpha); // ブレンドモードを指定（enum定数を利用）
+
     s3d::Point pos = board.toImagePos(Cursor::Pos());         // カーソル座標をイメージ座標に変換
-    if (board.checkRange(pos)) {                              // イメージの範囲内かどうかをチェック
-        board.mImg[pos].set(s3d::Palette::Cyan);              // 点をレンダリング（添え字範囲に注意）
-        Circle(pos, 3.0).overwrite(board.mImg, Palette::Red); // mImgはs3d::Image型と同じ扱いが可能
+    if (board.checkRange(pos)) {                              // イメージの座標範囲内かどうかをチェック
+        board.renderDot(pos, Palette::Blue);                  // 点をレンダリング（座標範囲に注意）
+        board.mImg[pos].set(Palette::Green);                  // mImgに直接書き込むことも可能
+        Circle(pos, 3.0).overwrite(board.mImg, Palette::Red); // 他のs3dメソッドとの組み合わせ
     }
-    board.mBoardPos = { 0.0, 5.0 };            // ボードをスクロール
-    board.setScale(2.0);                       // ズーム
-    board.draw();                              // ドロー
-    board.setSize(48, 36);                     // ドットサイズを変更（ボードは白紙になる。高負荷注意）
-    board.mVisible = false;                    // 非表示にする
+    board.mBoardPos = { 0.0, 5.0 };         // ボードをスクロール
+    board.setScale(2.0);                    // ズーム
+    board.draw();                           // ドロー
+
+    board.changeSize(48, 36);               // ドットサイズを変更（ボードは白紙になる。高負荷注意）
+    board.mVisible = false;                 // 非表示にする
 **************************************************************************************************/
 
 #pragma once
@@ -36,16 +49,19 @@ KotsubuPixelBoard board(32, 24, 10.0);         // 32x24ドット、ズーム率1
 class KotsubuPixelBoard
 {
 public:
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  【公開】定数、フィールド、コンストラクタ、一般メソッド
+    //
+
     // 【公開定数】ブレンドモード
-    static enum class EnumBlendMode { Default, Alpha, Add, Add2 };
+    static enum class EnumBlendMode { Default, Alpha, Additive, AdditiveSoft, Multiple };
 
-
+    
     // 【公開フィールド】
     s3d::Vec2  mBoardPos;  // ピクセルボードの左上位置
-    s3d::Image mImg;       // 描画用イメージ。これに直接.set()などで書き込んで.draw()する
+    s3d::Image mImg;       // 描画用イメージ。直接操作が可能
     bool       mVisible;   // 表示非表示の切り替え
-    // 【テスト】
-    EnumBlendMode mBlendMode;
 
 
     // 【コンストラクタ】
@@ -79,8 +95,8 @@ public:
 
 
 
-    // 【メソッド】サイズを変更（単位はドット。高負荷）
-    // サイズが変わらない場合は何もしない。変わる場合は描画イメージはクリア
+    // 【メソッド】イメージのサイズを変更（単位はドット。高負荷）
+    // サイズが変わらない場合は何もしない。変わる場合は描画イメージはクリアされる
     // ＜注意＞ 連続的に異なるサイズを設定すると、高負荷のためエラー落ちする
     void changeSize(size_t width, size_t height)
     {
@@ -182,23 +198,28 @@ public:
     {
         switch (blendMode) {
         case EnumBlendMode::Alpha:
-            break;
+            mFunctor = FuncBlender_alpha();        break;
 
-        case EnumBlendMode::Add:
-            mFunctor = FuncBlender_add();     break;
+        case EnumBlendMode::Additive:
+            mFunctor = FuncBlender_additive();     break;
 
-        case EnumBlendMode::Add2:
-            mFunctor = FuncBlender_add2();    break;
+        case EnumBlendMode::AdditiveSoft:
+            mFunctor = FuncBlender_additiveSoft(); break;
+
+        case EnumBlendMode::Multiple:
+            mFunctor = FuncBlender_multiple();     break;
 
         default:
-            mFunctor = FuncBlender_default(); break;
+            mFunctor = FuncBlender_default();      break;
         }
-
-        // 【テスト】
-        mBlendMode = blendMode;
     }
 
 
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  【公開】レンダリング系メソッド
+    //
 
     // 【メソッド】点をレンダリング
     void renderDot(s3d::Point pos, s3d::ColorF col)
@@ -208,9 +229,288 @@ public:
 
 
 
+    // 【メソッド】線分をレンダリング
+    void renderLine(s3d::Point startPos, s3d::Point endPos, s3d::ColorF col)
+    {   
+        // ◎ブレゼンハムアルゴリズム
+        // 終点を初期位置として始める
+        s3d::Point now = endPos;
+        // xとyそれぞれの、距離（絶対値）と進むべき方向（正負）を求める
+        s3d::Point dist, step;
+        if (endPos.x >= startPos.x)
+            { dist.x = endPos.x - startPos.x; step.x = -1; }
+        else
+            { dist.x = startPos.x - endPos.x; step.x = 1; }
+        if (endPos.y >= startPos.y)
+            { dist.y = endPos.y - startPos.y; step.y = -1; }
+        else
+            { dist.y = startPos.y - endPos.y; step.y = 1; }
+        // 誤差の判定時に四捨五入する、かつ整数で扱うため、関連パラメータを2倍する
+        s3d::Point dist2 = dist * 2;
+
+
+        if (dist.x >= dist.y) {
+            // x基準
+            s3d::int32 e = dist.x;  // 誤差の初期値（四捨五入のために閾値/2とする）
+            for (;;) {
+                // 現在位置に点を描く
+                mFunctor(mImg, now, col);
+
+                // 始点なら終了
+                if (now.x == startPos.x) break;
+
+                // xを「1ドット」移動
+                now.x += step.x;
+
+                // 誤差を蓄積
+                e += dist2.y;
+
+                // 誤差がたまったら
+                if (e >= dist2.x) {
+                    // yを「1ドット」移動
+                    now.y += step.y;
+                    // 誤差をリセット。超過分を残すのがミソ
+                    e -= dist2.x;
+                }
+            }
+        }
+
+        else {
+            // y基準
+            s3d::int32 e = dist.y;
+            for (;;) {
+                mFunctor(mImg, now, col);
+
+                if (now.y == startPos.y) break;
+                now.y += step.y;
+                e += dist2.x;
+
+                if (e >= dist2.y) {
+                    now.x += step.x;
+                    e -= dist2.y;
+                }
+            }
+        }
+    }
+
+
+
+    // 【メソッド】線分をレンダリング（疑似アンチエイリアシング付き）
+    void renderLineAA(s3d::Point startPos, s3d::Point endPos, s3d::ColorF col, double aaColorRate = 0.5)
+    {
+        // 終点を初期位置として始める
+        s3d::Point now = endPos;
+        // xとyそれぞれの、距離（絶対値）と進むべき方向（正負）を求める
+        s3d::Point dist, step;
+        if (endPos.x >= startPos.x)
+            { dist.x = endPos.x - startPos.x; step.x = -1; }
+        else
+            { dist.x = startPos.x - endPos.x; step.x = 1; }
+        if (endPos.y >= startPos.y)
+            { dist.y = endPos.y - startPos.y; step.y = -1; }
+        else
+            { dist.y = startPos.y - endPos.y; step.y = 1; }
+        // 誤差の判定時に四捨五入する、かつ整数で扱うため、関連パラメータを2倍する
+        s3d::Point dist2 = dist * 2;
+        // AA部分の通常部分に対する色の割合
+        if (aaColorRate < 0.0) aaColorRate = 0.0;
+        if (aaColorRate > 1.0) aaColorRate = 1.0;
+        // AA部分の色
+        s3d::ColorF aaCol = s3d::ColorF(col, col.a * aaColorRate);
+
+
+        if (dist.x >= dist.y) {
+            // x基準
+            s3d::int32 e = dist.x;  // 誤差の初期値（四捨五入のために閾値/2とする）
+            for (;;) {
+                // 現在位置に点を描く
+                mFunctor(mImg, now, col);
+
+                // 始点なら終了
+                if (now.x == startPos.x) break;
+
+                // xを「1ドット」移動
+                now.x += step.x;
+
+                // 誤差を蓄積
+                e += dist2.y;
+
+                // 誤差がたまったら
+                if (e >= dist2.x) {
+                    mFunctor(mImg, now, aaCol);                        // 疑似AA
+
+                    // yを「1ドット」移動
+                    now.y += step.y;
+
+                    mFunctor(mImg, { now.x - step.x, now.y }, aaCol);  // 疑似AA
+
+                    // 誤差をリセット。超過分を残すのがミソ
+                    e -= dist2.x;
+                }
+            }
+        }
+
+        else {
+            // y基準
+            s3d::int32 e = dist.y;
+            for (;;) {
+                mFunctor(mImg, now, col);
+
+                if (now.y == startPos.y) break;
+                now.y += step.y;
+                e += dist2.x;
+
+                if (e >= dist2.y) {
+                    mFunctor(mImg, now, aaCol);
+                    now.x += step.x;
+
+                    mFunctor(mImg, { now.x, now.y - step.y }, aaCol);
+                    e -= dist2.y;
+                }
+            }
+        }
+    }
+
+
+
+    // 【関数】線分をレンダリング（始点からフェードインする。疑似アンチエイリアシング付き）
+    void renderLineFadein(s3d::Point startPos, s3d::Point endPos, s3d::ColorF col,
+                          double fadingSectionRate = 0.5, double aaColorRate = 0.5)
+    {
+        // 終点を初期位置として始める
+        s3d::Point now = endPos;
+        // xとyそれぞれの、距離（絶対値）と進むべき方向（正負）を求める
+        s3d::Point dist, step;
+        if (endPos.x >= startPos.x)
+            { dist.x = endPos.x - startPos.x; step.x = -1; }
+        else
+            { dist.x = startPos.x - endPos.x; step.x = 1; }
+        if (endPos.y >= startPos.y)
+            { dist.y = endPos.y - startPos.y; step.y = -1; }
+        else
+            { dist.y = startPos.y - endPos.y; step.y = 1; }
+        // 誤差の判定時に四捨五入する、かつ整数で扱うため、関連パラメータを2倍する
+        s3d::Point dist2 = dist * 2;
+        // AA部分の通常部分に対する色の割合
+        if (aaColorRate < 0.0) aaColorRate = 0.0;
+        if (aaColorRate > 1.0) aaColorRate = 1.0;
+        // AA部分の色
+        s3d::ColorF aaCol = s3d::ColorF(col, col.a * aaColorRate);
+        // フェード区間の割合
+        if (fadingSectionRate < 0.0) fadingSectionRate = 0.0;
+        if (fadingSectionRate > 1.0) fadingSectionRate = 1.0;
+
+
+        if (dist.x >= dist.y) {
+            // ◎◎ x基準
+            s3d::int32 e        = dist.x;  // 誤差の初期値（四捨五入のために閾値/2とする）
+            s3d::int32 decayLen = (endPos.x - startPos.x) * fadingSectionRate;  // フェード区間の長さ
+            s3d::int32 splitX   = startPos.x + decayLen;                        // 分割点x
+
+            // ◎ 終点xから分割点xまでループ（通常のAA付き線分の処理）
+            for (;;) {
+                // 現在位置に点を描く
+                mFunctor(mImg, now, col);
+
+                // 分割点ならループを抜ける
+                if (now.x == splitX) break;
+
+                // xを「1ドット」移動
+                now.x += step.x;
+
+                // 誤差を蓄積
+                e += dist2.y;
+
+                // 誤差がたまったら
+                if (e >= dist2.x) {
+                    mFunctor(mImg, now, aaCol);                        // 疑似AA
+
+                    // yを「1ドット」移動
+                    now.y += step.y;
+
+                    mFunctor(mImg, { now.x - step.x, now.y }, aaCol);  // 疑似AA
+
+                    // 誤差をリセット。超過分を残すのがミソ
+                    e -= dist2.x;
+                }
+            }
+
+            // 始点なら終了
+            if (now.x == startPos.x) return;
+
+            // ◎ 分割点xから始点xまでループ（ここがフェードする）
+            double alphaFadeVol = col.a / (1 + std::abs(decayLen));  // アルファのフェード量
+            for (;;) {
+                // 初回の重複描画を避けるためフローを変更
+                now.x += step.x;
+                e += dist2.y;
+
+                col.a -= alphaFadeVol;  // アルファをフェードアウト
+
+                if (e >= dist2.x) {
+                    mFunctor(mImg, now, ColorF(col, col.a * aaColorRate));
+                    now.y += step.y;
+                    mFunctor(mImg, { now.x - step.x, now.y }, ColorF(col, col.a * aaColorRate));
+                    e -= dist2.x;
+                }
+
+                mFunctor(mImg, now, col);
+                if (now.x == startPos.x) break;
+            }
+        }
+
+        else {
+            // ◎◎ y基準
+            s3d::int32 e        = dist.y;
+            s3d::int32 decayLen = (endPos.y - startPos.y) * fadingSectionRate;
+            s3d::int32 splitY   = startPos.y + decayLen;
+
+            for (;;) {
+                mFunctor(mImg, now, col);
+                if (now.y == splitY) break;
+
+                now.y += step.y;
+                e += dist2.x;
+
+                if (e >= dist2.y) {
+                    mFunctor(mImg, now, aaCol);
+                    now.x += step.x;
+                    mFunctor(mImg, { now.x, now.y - step.y }, aaCol);
+                    e -= dist2.y;
+                }
+            }
+            if (now.y == startPos.y) return;
+        
+            double alphaFadeVol = col.a / (1 + std::abs(decayLen));
+            for (;;) {
+                now.y += step.y;
+                e += dist2.x;
+
+                col.a -= alphaFadeVol;
+
+                if (e >= dist2.y) {
+                    mFunctor(mImg, now, ColorF(col, col.a * aaColorRate));
+                    now.x += step.x;
+                    mFunctor(mImg, { now.x, now.y - step.y }, ColorF(col, col.a * aaColorRate));
+                    e -= dist2.y;
+                }
+
+                mFunctor(mImg, now, col);
+                if (now.y == startPos.y) break;
+            }
+        }
+    }
+
+
+
 
 
 private:
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  【内部】レンダリング用の関数オブジェクト、フィールド
+    //
+
     // 【内部関数オブジェクト】ブレンド種類別、イメージの1点を書き変える処理群
     static struct FuncBlender_default {
         void operator()(s3d::Image& img, s3d::Point pos, s3d::ColorF col)
@@ -221,28 +521,57 @@ private:
 
 
 
-    static struct FuncBlender_add {
+    static struct FuncBlender_alpha {
         void operator()(s3d::Image& img, s3d::Point pos, s3d::ColorF col)
         {
-            s3d::ColorF src = img[pos];                 // 現時点の色をスポイト
-            s3d::ColorF dst = {  src.r + col.r * col.a, // 塗りたい色と合成
-                                 src.g + col.g * col.a,
-                                 src.b + col.b * col.a,
-                                (src.a + col.a) * 0.5  };
-            img[pos].set(dst);                          // 混ぜた色をセット
+            s3d::ColorF src = img[pos];                        // 現時点の色をスポイト
+            double alpha = 1.0 - col.a;
+            s3d::ColorF dst = { src.r * alpha + col.r * col.a, // 塗りたい色と合成
+                                src.g * alpha + col.g * col.a,
+                                src.b * alpha + col.b * col.a,
+                                src.a * alpha + col.a };
+            img[pos].set(dst);                                 // 混ぜた色をセット
         }
     };
 
 
 
-    static struct FuncBlender_add2 {
+    static struct FuncBlender_additive {
         void operator()(s3d::Image& img, s3d::Point pos, s3d::ColorF col)
         {
             s3d::ColorF src = img[pos];
             s3d::ColorF dst = { src.r + col.r * col.a,
                                 src.g + col.g * col.a,
                                 src.b + col.b * col.a,
-                                src.a + col.a };       // 本来は違うが見栄えがよい（キラキラする）
+                                src.a + col.a };
+            img[pos].set(dst);
+        }
+    };
+
+
+
+    static struct FuncBlender_additiveSoft {
+        void operator()(s3d::Image& img, s3d::Point pos, s3d::ColorF col)
+        {
+            s3d::ColorF src = img[pos];
+            s3d::ColorF dst = { src.r + col.r * col.a,
+                                src.g + col.g * col.a,
+                                src.b + col.b * col.a,
+                               (src.a + col.a) * 0.5 };  // 平均をとってみた
+            img[pos].set(dst);
+        }
+    };
+
+
+
+    static struct FuncBlender_multiple {
+        void operator()(s3d::Image& img, s3d::Point pos, s3d::ColorF col)
+        {
+            s3d::ColorF src = img[pos];
+            s3d::ColorF dst = { src.r * col.r,
+                                src.g * col.g,
+                                src.b * col.b,
+                                src.a * col.a };
             img[pos].set(dst);
         }
     };
